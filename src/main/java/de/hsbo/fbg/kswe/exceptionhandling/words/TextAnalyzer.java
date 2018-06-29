7cf0830e37c23d97c171df7852ad45533011f4ea
NathiPi <nathalie.pitz@hs-bochum.de>
2018-06-29 12:22:20 +0200

    Created Text Analyzer with 100% Code Coverage

--- src/main/java/de/hsbo/fbg/kswe/exceptionhandling/words/TextAnalyzer.java
+++ src/main/java/de/hsbo/fbg/kswe/exceptionhandling/words/TextAnalyzer.java
@@ -10,8 +10,22 @@ public class TextAnalyzer {
     public TextAnalyzer() {
     }
 
-    public String findLongestWord(String text) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String findLongestWord(String input) {
+    	String[] word = input.split(" ");
+		// Anlegen eines leeren Strings fuer das laengste Wort
+        String longest = "";
+        // Durchlaufen aller Woerter
+        for(int i=0; i<word.length; i++){
+        	// Vergleich der Laenge eines Wortes mit der Laenge des Strings fuer das laengste Worte
+        	 if (word[i].length() > longest.length()){
+        		 // Fuellen des Strings fuer das laengste Wort mit dem aktuell laengsten Wort
+                 longest = word[i];
+                }
+        }
+        System.out.println(longest);
+        System.out.println(longest.length());
+        
+        return longest;
     }
 
 }
